5ec4b31f22437b0a6147fe9cd9908fec5bfb4c88
Bruno Ferreira <bmibferreira@gmail.com>
2020-11-23 00:28:37 +0000

    add ec2 instance

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 module "ec2_cluster" {
   source           = "../modules/ec2-cluster"
   name             = "awesome-ec2-cluster"
-  instances_number = 3
+  instances_number = 4
   vpc_id           = module.vpc.vpc_id
   subnet_id        = module.vpc.private_subnets[0]
   instance_type    = "t2.micro"
