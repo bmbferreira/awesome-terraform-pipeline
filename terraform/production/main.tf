f073371803b26bec5ecbaf2076d2030f41e01e0e
Bruno Ferreira <bmibferreira@gmail.com>
2020-12-08 23:10:04 +0000

    add s3 bucket

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -9,7 +9,7 @@ module "ec2_cluster" {
   instances_number = 1
   vpc_id           = module.vpc.vpc_id
   subnet_id        = module.vpc.private_subnets[0]
-  instance_type    = "t3.micro"
+  instance_type    = "t3.xlarge"
   ebs_type         = "gp2"
   ami              = "ami-09b9e380df60300c8"
 }
